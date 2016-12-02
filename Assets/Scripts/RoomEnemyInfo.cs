ea0827d7f80e00818709d169141d9bb093fbed48
samuel <subjectkpop@gmail.com>
2016-12-02 11:05:30 +0800

    - Fixed crystals prefab - Check for room lighting - Add spawn points

---
+++ Assets/Scripts/RoomEnemyInfo.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoomEnemyInfo : MonoBehaviour {
+    //room capacity size
+    public int RoomEnemStr = 10;
+
+    public bool IsBossRoom = false;
+	
+}
