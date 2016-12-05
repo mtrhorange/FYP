89d852e04a643f9b1415b35e8359c13eafad6a73
Mark Tan <mtrhorange@hotmail.com>
2016-12-05 20:29:28 +0800

    Cave rooms

--- Assets/Scripts/Managers/LevelManager.cs
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -47,7 +47,6 @@ public class LevelManager : MonoBehaviour {
 
 	public void ChangeRoom(Door nextDoor) {
 
-		nextDoor.currentRoom.gameObject.SetActive (true);
 
 
 	}
--- Assets/Scripts/Room.cs
+++ Assets/Scripts/Room.cs
@@ -4,10 +4,6 @@ using System.Collections;
 public class Room : MonoBehaviour {
 
 
-	public Door exit;
-
-	public bool canLeft, canRight, canUp, canDown;
-
 	public Transform spawnPoint1, spawnPoint2;
 	// Use this for initialization
 	void Start () {
