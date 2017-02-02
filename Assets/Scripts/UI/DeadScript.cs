9f890f90ce629e0df3b3f209d29082b3d1d94346
Zhimin <youzm40@gmail.com>
2017-02-02 16:21:09 +0800

    updates and shit from zm

--- Assets/Scripts/UI/DeadScript.cs
+++ Assets/Scripts/UI/DeadScript.cs
@@ -8,6 +8,7 @@ public class DeadScript : MonoBehaviour {
     public Image cover;
     public GameObject de;
     private bool fadedIn;
+    private bool ded;
 
 	// Use this for initialization
 	void Start () {
@@ -16,29 +17,49 @@ public class DeadScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if(black.color.a < 1f && !fadedIn)
+
+        if (GameManager.instance.twoPlayers)
         {
-            black.color = new Color(0, 0, 0, black.color.a + Time.deltaTime * 0.5f);
-            cover.color = new Color(0, 0, 0, black.color.a + Time.deltaTime * 0.5f);
-            if(black.color.a > 1f)
+            if(GameManager.instance.player1.Health <= 0 && GameManager.instance.player2.Health <= 0)
             {
-                fadedIn = true;
+                ded = true;
             }
         }
         else
         {
-            de.SetActive(true);
-           
-            if (cover.color.a < 0f)
+            if (GameManager.instance.player1.Health <= 0)
             {
-                Destroy(cover.gameObject);
+                ded = true;
+            }
+        }
+
+
+        if (ded)
+        {
+            
+            if (black.color.a < 1f && !fadedIn)
+            {
+                black.color = new Color(0, 0, 0, black.color.a + Time.deltaTime * 0.5f);
+                cover.color = new Color(0, 0, 0, black.color.a + Time.deltaTime * 0.5f);
+                if (black.color.a > 1f)
+                {
+                    fadedIn = true;
+                }
             }
             else
             {
-                cover.color = new Color(0, 0, 0, cover.color.a - Time.deltaTime * 0.5f);
+                de.SetActive(true);
+
+                if (cover.color.a < 0f)
+                {
+                    Destroy(cover.gameObject);
+                }
+                else
+                {
+                    cover.color = new Color(0, 0, 0, cover.color.a - Time.deltaTime * 0.5f);
+                }
             }
         }
-         
-        
-	}
+
+    }
 }
